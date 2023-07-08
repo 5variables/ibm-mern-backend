76e421a678e3773c36a5180a4bea42917cc9fb95
notna4 <cristicanceal@gmail.com>
2023-07-08 19:51:23 +0300

    refactored code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const database = require('./config/database');
 const eventRoutes = require('./routes/eventRoutes');
+const registerRoutes = require('./routes/registerRoutes');
+
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/', eventRoutes);
+
+app.use('/events', eventRoutes);
+
+app.use('/register', registerRoutes);
 
 const port = 3001;
 app.listen(port, () => {
--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -2,17 +2,7 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
 
-router.get('/', eventController.getEvents);
+router.get('/display', eventController.getEvents);
 router.post('/create', eventController.createEvent);
 
-router.post('/register', eventController.registerUser);
-router.post('/login', eventController.loginUser);
-
-router.post('/verify-token', eventController.verifyToken);
-
 module.exports = router;
-
-
-/* 
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGE3MWQzYzE5N2YzMjViZTE4ODI3ODYiLCJpYXQiOjE2ODg2NzM2MjR9.v0Aj1-ifUV9fcl86p-UUNldjuAtk94aVxaofT-2yDdE
-*/
